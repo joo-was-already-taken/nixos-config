8568fee3403f7a947890220240ebeb9c50a5b49d
joo-was-already-taken <trackpointus@protonmail.com>
2024-09-28 16:30:38 +0200

    add tuigreet

---
+++ system-modules/login-manager/tuigreet.nix
@@ -0,0 +1,13 @@
+{ lib, pkgs, userSettings, session, ... }:
+
+{
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${session}";
+        user = userSettings.userName;
+      };
+    };
+  };
+}
