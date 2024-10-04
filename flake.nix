90be855128ee72bfe2ed591227b41cb10278d805
joo-was-already-taken <trackpointus@protonmail.com>
2024-10-04 13:25:11 +0200

    more changes

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
-      url = "github:danth/stylix";
+      url = "github:danth/stylix/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
