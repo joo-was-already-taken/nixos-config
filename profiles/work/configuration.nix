353ea480c987058fdddf56cf769d5d0e10acff1e
joo-was-already-taken <trackpointus@protonmail.com>
2024-10-05 19:51:11 +0200

    fix wireless

--- profiles/work/configuration.nix
+++ profiles/work/configuration.nix
@@ -34,15 +34,12 @@
   users.defaultUserShell = pkgs.zsh;
   programs.zsh.enable = true;
 
-  networking.hostName = systemSettings.hostName; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = systemSettings.hostName;
+  networking.networkmanager.enable = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Warsaw";
