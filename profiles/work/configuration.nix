dd099349f32d32c006ebde42085b78735a206e7f
joo-was-already-taken <trackpointus@protonmail.com>
2024-10-05 19:46:52 +0200

    enable wireless

--- profiles/work/configuration.nix
+++ profiles/work/configuration.nix
@@ -35,7 +35,7 @@
   programs.zsh.enable = true;
 
   networking.hostName = systemSettings.hostName; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
