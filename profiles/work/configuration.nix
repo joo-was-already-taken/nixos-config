3a87bd23fb00b907041463f015d5f8125acdfa70
joo-was-already-taken <trackpointus@protonmail.com>
2024-11-28 13:45:57 +0100

    enable opengl

--- profiles/work/configuration.nix
+++ profiles/work/configuration.nix
@@ -93,6 +93,12 @@
     jack.enable = true;
   };
 
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
