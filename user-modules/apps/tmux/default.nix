0ee83d728bffddc89588f9c77b6d07864e5c6b66
joo-was-already-taken <trackpointus@protonmail.com>
2024-12-01 13:48:44 +0100

    add restoring session for tmux and nvim

--- user-modules/apps/tmux/default.nix
+++ user-modules/apps/tmux/default.nix
@@ -5,7 +5,9 @@ in {
   options.modules.${moduleName}.enable = lib.mkEnableOption moduleName;
 
   config = lib.mkIf config.modules.${moduleName}.enable {
-    programs.tmux = {
+    programs.tmux = let
+      resurrectDirPath = "~/.tmux/resurrect/";
+    in {
       enable = true;
       
       plugins = with pkgs.tmuxPlugins; [
@@ -17,6 +19,15 @@ in {
             bind -T copy-mode-vi v send-keys -X begin-selection
             bind -T copy-mode-vi C-v send-keys -X rectangle-toggle
             bind -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+          '';
+        }
+        {
+          plugin = resurrect;
+          extraConfig = ''
+            set -g @resurrect-strategy-vim 'session'
+            set -g @resurrect-strategy-nvim 'session'
+            set -g @resurrect-capture-pane-contents 'on'
+            set -g @resurrect-dir ${resurrectDirPath}
           '';
         }
       ];
