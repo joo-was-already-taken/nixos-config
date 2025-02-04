afeb9c285cc316c37970f27de1ce2a0a65b751a2
joo-was-already-taken <trackpointus@protonmail.com>
2025-02-04 11:32:08 +0100

    setup smart borders in hyprland

--- user-modules/wm/hyprland/default.nix
+++ user-modules/wm/hyprland/default.nix
@@ -185,6 +185,20 @@ in {
           initial_workspace_tracking = 1;
         };
 
+        workspace = [
+          "w[tv1], gapsout:0, gapsin:0"
+          "f[1], gapsout:0, gapsin:0"
+        ];
+
+        windowrulev2 = [
+          # "suppressevent maximize, class:.*"
+          "bordersize 0, floating:0, onworkspace:w[tv1]"
+          "rounding 0, floating:0, onworkspace:w[tv1]"
+          "bordersize 0, floating:0, onworkspace:f[1]"
+          "rounding 0, floating:0, onworkspace:f[1]"
+        ];
+
+
         cursor = {
           inactive_timeout = 16;
         };
@@ -212,8 +226,6 @@ in {
         #   name = "epic-mouse-v1";
         #   sensitivity = -0.5;
         # };
-
-        windowrulev2 = "suppressevent maximize, class:.*";
 
         bind = [
           "$mod, F, exec, $webBrowser"
