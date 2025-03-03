cef6ff0b5eef4f2d0185f8d922445e1b7d7ad9a0
joo-was-already-taken <trackpointus@protonmail.com>
2025-03-03 10:24:08 +0100

    nvim tweaks

--- user-modules/apps/nvim/default.nix
+++ user-modules/apps/nvim/default.nix
@@ -100,131 +100,42 @@ in {
           '';
         }
 
-        (nvim-treesitter.withPlugins (plugins: with plugins; [
-          tree-sitter-nix
-          tree-sitter-lua
-          tree-sitter-bash
-          tree-sitter-gitignore
-          tree-sitter-python
-          tree-sitter-markdown
-          tree-sitter-markdown_inline
-          tree-sitter-make
-          tree-sitter-regex
-          tree-sitter-toml
-          tree-sitter-yaml
-          tree-sitter-json
-          tree-sitter-vim
-          tree-sitter-vimdoc
-          tree-sitter-rust
-          tree-sitter-haskell
-          tree-sitter-go
-          tree-sitter-html
-          tree-sitter-c
-          tree-sitter-cpp
-          tree-sitter-cmake
-          tree-sitter-zig
-          tree-sitter-java
-        ]))
+        {
+          plugin = (nvim-treesitter.withPlugins (plugins: with plugins; [
+            tree-sitter-nix
+            tree-sitter-lua
+            tree-sitter-bash
+            tree-sitter-gitignore
+            tree-sitter-python
+            tree-sitter-markdown
+            tree-sitter-markdown_inline
+            tree-sitter-make
+            tree-sitter-regex
+            tree-sitter-toml
+            tree-sitter-yaml
+            tree-sitter-json
+            tree-sitter-vim
+            tree-sitter-vimdoc
+            tree-sitter-rust
+            tree-sitter-haskell
+            tree-sitter-go
+            tree-sitter-html
+            tree-sitter-c
+            tree-sitter-cpp
+            tree-sitter-cmake
+            tree-sitter-zig
+            tree-sitter-java
+          ]));
+          config = lua /*lua*/ ''
+            require("nvim-treesitter").setup({
+              highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = true,
+              },
+            })
+          '';
+        }
       ];
     };
-
-    # home.file.".config/nvim/init.lua".text = /*lua*/ ''
-    #   require()
-    # '';
-    # home.file.".config/nvim/lua" = {
-    #   source = ./lua;
-    #   recursive = true;
-    # };
-
-    # home.file.".config/clangd/config.yaml".text = /*yaml*/ ''
-    #   CompileFlags:
-    #     Add: [-std=c++23]
-    # '';
-
-  #   programs.nixvim = {
-  #     enable = true;
-  #     defaultEditor = true;
-  #
-  #     # nixpkgs.useGlobalPackages = true;
-  #
-  #     enableMan = true;
-  #
-  #     # colorschemes.gruvbox.enable = true;
-  #     colorschemes.base16 = {
-  #       enable = true;
-  #       colorscheme = styleSettings.withHash.editorColors;
-  #     };
-  #
-  #     clipboard = {
-  #       register = "unnamedplus";
-  #       providers.wl-copy.enable = true;
-  #     };
-  #
-  #     globals = {
-  #       mapleader = " ";
-  #       maplocal = " ";
-  #     };
-  #
-  #     opts = {
-  #       # Indentation
-  #       tabstop = 2;
-  #       shiftwidth = 2;
-  #       softtabstop = 2;
-  #       expandtab = true;
-  #       smarttab = true;
-  #       smartindent = true;
-  #       autoindent = true;
-  #       wrap = false;
-  #       # Search
-  #       incsearch = true;
-  #       ignorecase = true;
-  #       smartcase = true;
-  #       hlsearch = true;
-  #       # Appearance
-  #       number = true;
-  #       relativenumber = true;
-  #       termguicolors = true;
-  #       signcolumn = "yes";
-  #       cmdheight = 1;
-  #       scrolloff = 10;
-  #       completeopt = "menuone,noinsert,noselect";
-  #       showtabline = 0; # never
-  #       listchars = "trail:·"; # trailing whitespace
-  #       list = true;
-  #       # Behaviour
-  #       hidden = true;
-  #       errorbells = false;
-  #       swapfile = false;
-  #       backup = false;
-  #       backspace = "indent,eol,start";
-  #       splitright = true;
-  #       splitbelow = true;
-  #       autochdir = false;
-  #       mouse = "a";
-  #       modifiable = true;
-  #       encoding = "UTF-8";
-  #     };
-  #
-  #     autoCmd = [
-  #       {
-  #         # Fix noice search popup border color
-  #         callback.__raw = ''
-  #           function()
-  #             local get_hl = function(hl) return vim.api.nvim_get_hl_by_name(hl, true) end
-  #             local fg = get_hl("NoiceCmdlinePopupBorderSearch").foreground
-  #             local bg = get_hl("NoiceCmdlinePopup").background
-  #             vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorderSearch", { fg = fg, bg = bg })
-  #           end
-  #         '';
-  #         event = [ "VimEnter" ];
-  #       }
-  #     ];
-  #
-  #     keymaps = import ./keymaps.nix;
-  #     plugins = (import ./plugins.nix args).plugins;
-  #     extraPlugins = (import ./plugins.nix args).extraPlugins;
-  #
-  #     extraConfigLua = (import ./plugins.nix args).extraConfigLua;
-  #   };
   };
 }
