0b751ec72908ce913333451286cd5814384045c5
joo-was-already-taken <trackpointus@protonmail.com>
2025-05-19 14:23:20 +0200

    fix alacritty colors

--- user-modules/apps/alacritty.nix
+++ user-modules/apps/alacritty.nix
@@ -24,30 +24,9 @@ in {
           lines = 32;
         };
 
-        # colors.primary.foreground = lib.mkForce
-        #   config.lib.stylix.colors.withHashtag.base06;
-
-        # font = let
-        #   family = config.stylix.fonts.monospace.name;
-        # in lib.mkForce { # override stylix settings
-        #   normal = {
-        #     inherit family;
-        #     style = "Medium";
-        #   };
-        #   bold = {
-        #     inherit family;
-        #     style = "ExtraBold";
-        #   };
-        #   # italic = {
-        #   #   inherit family;
-        #   #   style = "MediumItalic";
-        #   # };
-        #   # bold_italic = {
-        #   #   inherit family;
-        #   #   style = "BoldItalic";
-        #   # };
-        # };
-      }; # settings
+        colors.primary.foreground = lib.mkForce
+          config.lib.stylix.colors.withHashtag.base06;
+      };
     };
   };
 }
