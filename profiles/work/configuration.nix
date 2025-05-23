4c4ac007bb60acb250f9b3e9adaf7bf5c3023988
joo-was-already-taken <trackpointus@protonmail.com>
2025-05-23 17:11:11 +0200

    change number locale

--- profiles/work/configuration.nix
+++ profiles/work/configuration.nix
@@ -51,14 +51,13 @@
   i18n.defaultLocale = "en_DK.UTF-8";
 
   i18n.extraLocaleSettings = {
-    # LC_ALL = "en_DK.UTF-8";
     LC_ADDRESS = "pl_PL.UTF-8";
     LC_IDENTIFICATION = "pl_PL.UTF-8";
-    LC_MEASUREMENT = "pl_PL.UTF-8";
+    LC_MEASUREMENT = "en_DK.UTF-8";
     LC_MONETARY = "pl_PL.UTF-8";
     LC_NAME = "pl_PL.UTF-8";
-    LC_NUMERIC = "pl_PL.UTF-8";
-    LC_PAPER = "pl_PL.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_DK.UTF-8";
     LC_TELEPHONE = "pl_PL.UTF-8";
     LC_TIME = "en_DK.UTF-8";
   };
