5a15549ac0d051600272e971d4612b8dfaca540f
joo-was-already-taken <trackpointus@protonmail.com>
2025-05-24 19:38:05 +0200

    changes ...

--- user-modules/wm/eww/scripts/default.nix
+++ user-modules/wm/eww/scripts/default.nix
@@ -4,20 +4,24 @@ rec {
     name = "hypr-fullscreenmode";
     text = builtins.readFile ./hypr-fullscreenmode.sh;
   };
+
   hyprListen = pkgs.writeShellApplication {
     name = "hypr-listen";
     runtimeInputs = [ pkgs.socat hyprFullscreenMode ];
     text = builtins.readFile ./hypr-listen.sh;
   };
+
   hyprWorkspaces = pkgs.writeShellApplication {
     name = "hypr-workspaces";
     runtimeInputs = [ pkgs.socat ];
     text = builtins.readFile ./hypr-workspaces.sh;
   };
+
   battery = pkgs.writeShellApplication {
     name = "battery";
     text = builtins.readFile ./battery.sh;
   };
+
   pulseVolume = pkgs.stdenv.mkDerivation {
     name = "pulse-volume-listener";
     propagatedBuildInputs = [
