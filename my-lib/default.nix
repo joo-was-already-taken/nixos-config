6a18da605b81cc590cf0457da9913e645ee71d13
joo-was-already-taken <trackpointus@protonmail.com>
2025-05-25 19:56:16 +0200

    eww: add text-progress bars

--- my-lib/default.nix
+++ my-lib/default.nix
@@ -9,14 +9,20 @@ let
       };
   in
     (colors: builtins.mapAttrs mapFn colors);
+  mapAttrsToString = fn: let
+    toList = attrs: lib.attrsets.mapAttrsToList fn attrs;
+  in attrs: lib.strings.concatStringsSep "\n" (toList attrs);
 in
 {
   mkColorsOption = mkAttrOptionMatchingStr "^(([0-9A-Za-z]{3})|([0-9A-Za-z]{6}))$";
   mkHashColorsOption = mkAttrOptionMatchingStr "^#(([0-9A-Za-z]{3})|([0-9A-Za-z]{6}))$";
 
-  gtkCssDefineColors = let
-    toList = colors: lib.attrsets.mapAttrsToList (k: v: "@define-color ${k} ${v};") colors;
-  in colors: lib.strings.concatStringsSep "\n" (toList colors);
+  # gtkCssDefineColors = let
+  #   toList = colors: lib.attrsets.mapAttrsToList (k: v: "@define-color ${k} ${v};") colors;
+  # in colors: lib.strings.concatStringsSep "\n" (toList colors);
+  # scssDefineColors = 
+  gtkCssDefineColors = mapAttrsToString (k: v: "@define-colors ${k} ${v};");
+  scssDefineColors = mapAttrsToString (k: v: "\$${k}: ${v};");
 
   importAll = args: paths: map (p: import p args) paths;
 
--- user-modules/wm/eww/default.nix
+++ user-modules/wm/eww/default.nix
@@ -1,11 +1,18 @@
-{ config, lib, pkgs, ... }@args:
+{ config, lib, myLib, pkgs, ... }@args:
 let
   moduleName = "eww";
   scripts = import ./scripts args;
+  defaultColors = with config.lib.stylix.colors.withHashtag; {
+    volume = base0E;
+    brightness = base0A;
+    battery = base0B;
+  };
+  colors = config.modules.${moduleName}.colors;
 in {
   options.modules.${moduleName} = {
     enable = lib.mkEnableOption moduleName;
     bar.enable = lib.mkEnableOption "enable bar widget";
+    colors = myLib.mkHashColorsOption defaultColors;
   };
 
   config = lib.mkIf config.modules.${moduleName}.enable {
@@ -25,9 +32,8 @@ in {
 
     fonts.fontconfig.enable = true;
 
-    home.file.".config/eww" = {
-      source = ./bar;
-      recursive = true;
-    };
+    home.file.".config/eww/eww.yuck".source = ./bar/eww.yuck;
+    home.file.".config/eww/eww.scss".text =
+      (myLib.scssDefineColors colors) + "\n" + builtins.readFile ./bar/eww.scss;
   };
 }
