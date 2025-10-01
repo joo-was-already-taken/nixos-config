f78c641019f8bfcd74698bdf92dd7102ab4220cd
joo-was-already-taken <trackpointus@protonmail.com>
2025-10-01 19:01:58 +0200

    change sddm theme

--- system-modules/display-managers/sddm.nix
+++ system-modules/display-managers/sddm.nix
@@ -1,17 +1,27 @@
 { pkgs, ... }:
 
 {
+  # environment.systemPackages = with pkgs; [
+  #   sddm-astronaut
+  # ];
+  # services.displayManager.sddm = {
+  #   enable = true;
+  #   wayland.enable = true;
+  #   package = pkgs.kdePackages.sddm;
+  #   theme = "sddm-astronaut-theme";
+  #   extraPackages = with pkgs.kdePackages; [
+  #     qtmultimedia
+  #   ];
+  # };
+
   environment.systemPackages = with pkgs; [
-    sddm-astronaut
+    where-is-my-sddm-theme
   ];
 
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
     package = pkgs.kdePackages.sddm;
-    theme = "sddm-astronaut-theme";
-    extraPackages = with pkgs.kdePackages; [
-      qtmultimedia
-    ];
+    theme = "where_is_my_sddm_theme";
   };
 }
