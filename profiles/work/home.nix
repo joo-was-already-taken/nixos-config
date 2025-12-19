576acd11d1acf4be4f0c840edce1435c69c7594c
joo-was-already-taken <trackpointus@protonmail.com>
2025-12-19 13:48:48 +0100

    change nixpkgs channel for winboat

--- profiles/work/home.nix
+++ profiles/work/home.nix
@@ -144,11 +144,13 @@ in {
 
     transmission_4-gtk
 
-    # TODO: use stable in 25.11
-    (unstable.winboat.overrideAttrs (old: {
-      # doesn't build without this
     appimage-run
 
+    # As of 17.12.2025 unstable version with podman support is broken.
+    # When podman support is fixed, remove docker group, otherwise needed
+    # for winboat.
+    # An additional npm flag is needed.
+    (winboat.overrideAttrs (old: {
       npmFlags = old.npmFlags or [] ++ [ "--legacy-peer-deps" ];
     }))
 
