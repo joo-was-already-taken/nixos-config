2434d9e1115df2cc33fceef55de81f08321d69f7
joo-was-already-taken <trackpointus@protonmail.com>
2026-01-29 21:56:48 +0100

    limit nix daemon cpu usage

--- profiles/work/configuration.nix
+++ profiles/work/configuration.nix
@@ -25,6 +25,9 @@
     trusted-substituters = [ "https://hyprland.cachix.org" ];
     trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
+  nix.daemonCPUSchedPolicy = "idle";
+  nix.daemonIOSchedClass = "idle";
+
 
   swapDevices = [
     {
